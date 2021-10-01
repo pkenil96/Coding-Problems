eddc5fc31840d11caa41b6c9adc990fcc9fffbaf
pkenil96 <kenspatel96@gmail.com>
2021-10-01 18:05:21 -0400

    updated L239 and added L98

--- Leetcode/L239SlidingWindowMaximum.java
+++ Leetcode/L239SlidingWindowMaximum.java
@@ -1,17 +1,47 @@
 class L239SlidingWindowMaximum {
     
-    // brute force method
-    public int[] solution0(int[] nums, int k){
-        
+    public static int[] solution0(int[] nums, int k){
+        // TLE: O(n*k)
+        int n = nums.length, index=0;
+        int[] result = new int[n-k+1];
+        for(int i=0; i<=n-k; i++){
+            int max = Integer.MIN_VALUE;
+            for(int j=i; j<i+k; j++){
+                max = Math.max(max, nums[j]);
+            }
+            result[index++]=max;
+        }
+        return result;
     }
     
-    // using the deque
-    public int[] solution1(int[] nums, int k){
+    // solution using max heap
+    public static int[] solution1(int[] nums, int k){
+        int n = nums.length;
+        int[] result = new int[n-k+1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> b[0]-a[0]);
+        // initializing the heap with k elements
+        for(int i=0; i<k; i++){
+            pq.add(new int[]{nums[i], i});
+        }
+        int index = 0;
+        result[index++] = pq.peek()[0];
         
+        for(int i=1; i<=n-k; i++){
+            pq.add(new int[]{nums[i+k-1], i+k-1});
+            int item = pq.peek()[0];
+            int idx = pq.peek()[1];
+            
+            while(pq.peek()[1] < i){
+                pq.poll();    
+            }
+            result[index++] = pq.peek()[0];
+        }
+        return result;
     }
     
-    // using the heap
-    public int[] solution2(int[] nums, int k){
+    
+    // solution using deque
+    public static int[] solution2(int[] nums, int k){
         
     }
     
