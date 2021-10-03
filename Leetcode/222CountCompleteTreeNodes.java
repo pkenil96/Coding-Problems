9c2a553a9c550220c74986a14443782e6f7bc6fd
pkenil96 <kenspatel96@gmail.com>
2021-10-03 17:19:01 -0400

    L222 added

---
+++ Leetcode/222CountCompleteTreeNodes.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class 222CountCompleteTreeNodes {
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftSubtreeNodeCount = countNodes(root.left);
+        int rightSubTreeNodeCount = countNodes(root.right);
+        return 1+leftSubtreeNodeCount+rightSubTreeNodeCount;
+    }
+}
