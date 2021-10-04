e2afec70b70107a68af8eb8c576d03305329fa97
pkenil96 <kenspatel96@gmail.com>
2021-10-03 22:31:13 -0400

    added l703

---
+++ Leetcode/L703KthLargestInStream.java
@@ -0,0 +1,29 @@
+class L703KthLargestInStream {
+    
+    int k;
+    PriorityQueue<Integer> pq;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>();
+        for(int i=0; i<nums.length; i++){
+            pq.offer(nums[i]);
+            if(pq.size() > k){
+                pq.poll();
+            }
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if(pq.size() > k){
+            pq.poll();
+        }
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
