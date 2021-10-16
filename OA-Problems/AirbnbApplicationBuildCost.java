d3abf495d019ca7948a236f11bdd0862e8f14c58
pkenil96 <kenspatel96@gmail.com>
2021-10-15 20:47:05 -0400

    Added template for OA problems

---
+++ OA-Problems/AirbnbApplicationBuildCost.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class AirbnbApplicationBuildCost{
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ OA-Problems/AmericanAirlinesIntersectionOfTwoArrays.java
@@ -0,0 +1,5 @@
+public class AmericanAirlinesIntersectionOfTwoArrays{
+	public static void main(String[] args) {
+		
+	}
+}
