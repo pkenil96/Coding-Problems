f0a4e986fdef2271a7ef66c75589c7e6d1bf2990
pkenil96 <kenspatel96@gmail.com>
2021-10-15 20:54:07 -0400

    added L20

---
+++ Leetcode/L20ValidParentheses.java
@@ -0,0 +1,22 @@
+class L20ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch == '('  || ch == '[' || ch == '{'){
+                stack.push(ch);
+            } else if(stack.isEmpty()){
+                return false;
+            } else {
+                char top = stack.pop();
+                if(ch == ')' && top != '('){
+                    return false;
+                } else if(ch == '}' && top != '{'){
+                    return false;
+                } else if(ch == ']' && top != '['){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
