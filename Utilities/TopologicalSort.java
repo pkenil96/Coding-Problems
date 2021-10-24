9e8ed22fe66b39b7b396d0c4997083b78d3bb1f6
pkenil96 <kenspatel96@gmail.com>
2021-10-24 12:45:43 -0400

    Added topological sort

---
+++ Utilities/TopologicalSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class TopologicalSort {
+        
+    public static void topologicalSort(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, Stack<Integer> stack){
+        visited[node] = true;
+        for(int curr: adj.get(node)){
+            if(visited[curr] == false){
+                topologicalSort(curr, adj, visited, stack);
+            }
+        }
+        stack.push(node);
+    }
+    
+    //Function to return list containing vertices in Topological order. 
+    public static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] output = new int[V];
+        boolean[] visited = new boolean[V];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<V; i++){
+            if(visited[i] == false){
+                topologicalSort(i, adj, visited, stack);
+            }
+        }
+        int index = 0;
+        while(!stack.isEmpty()){
+            output[index] = stack.pop();
+            index++;
+        }
+        //System.out.println(Arrays.toString(output));
+        return output;
+    }
+}
