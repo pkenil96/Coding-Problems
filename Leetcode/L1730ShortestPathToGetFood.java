533c596b5c766ed9dbdcec055730eef77300d6c6
pkenil96 <kenspatel96@gmail.com>
2021-10-27 10:42:41 -0400

    Added L1730

---
+++ Leetcode/L1730ShortestPathToGetFood.java
@@ -0,0 +1,57 @@
+class Pair{
+    int x;
+    int y;
+    Pair(int x1, int y1){
+        x=x1;
+        y=y1;
+    }
+}
+
+class L1730ShortestPathToGetFood {
+    
+    public int bfs(int src, int col, char[][] grid, boolean[][] visited){
+        int ans=0;
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(src, col));
+        visited[src][col] = true;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int p=0; p<size; p++){
+                int[] dr = {1, -1, 0, 0};
+                int[] dc = {0, 0, 1, -1};
+                Pair pair = q.poll();
+                for(int i=0; i<4; i++){
+                    int newX = pair.x + dr[i];
+                    int newY = pair.y + dc[i];
+                    if(newX < 0 || newX >= m || newY < 0 || newY >= n || grid[newX][newY] == 'X' || grid[newX][newY] == '*' || visited[newX][newY] == true){
+                        continue;
+                    }
+                    if(grid[newX][newY] == '#'){
+                        return ans+1;
+                    }
+                    visited[newX][newY]=true;    
+                    q.add(new Pair(newX, newY));
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+    
+    public int getFood(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == '*'){
+                    return bfs(i, j, grid, visited);
+                }
+            }
+        }
+        return -1;
+    }
+}
