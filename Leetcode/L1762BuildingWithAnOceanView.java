6bca6f7a1598f262a6677cef74cfddd871c5a1a4
pkenil96 <kenspatel96@gmail.com>
2021-10-27 09:29:34 -0400

    Added L1762

---
+++ Leetcode/L1762BuildingWithAnOceanView.java
@@ -0,0 +1,22 @@
+class L1762BuildingWithAnOceanView {
+    public int[] findBuildings(int[] heights) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = heights.length;
+        int max = heights[n-1];
+        int index = n-2;
+        list.add(n-1);
+        while(index >= 0){
+            if(heights[index] > max){
+                max = heights[index];
+                list.add(index);
+            }
+            index--;
+        }
+        int[] res = new int[list.size()];
+        int k=0;
+        for(int i=list.size()-1; i>=0; i--){
+            res[i] = list.get(k++);
+        }
+        return res;
+    }
+}
